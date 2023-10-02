2be47b99bee79763cf856adcc16cc2e8d3a2c8c4
Neiv Malaviya <90695450+Si6gma@users.noreply.github.com>
2023-10-02 12:26:35 +0530

    Added dots

--- facialRecog.py
+++ facialRecog.py
@@ -6,9 +6,11 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 # Open the webcam
 cap0 = cv2.VideoCapture(1)
 
+# Storing the resolution of the camera (in pixels)
 frame_width = int(cap0.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap0.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+# Displaying the resolution
 print(f"Video resolution: {frame_width}x{frame_height}")
 
 while True:
@@ -32,10 +34,17 @@ while True:
     # Draw rectangle around the faces and a red dot at the center
     for (x, y, w, h) in faces0:
         cv2.rectangle(frame0, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+        # Finding the center of the rectangle
         center_x, center_y = x + w//2, y + h//2
+
+        # Drawing a point at the center of the rectangle
         cv2.circle(frame0, (center_x, center_y), radius=2, color=(0, 0, 255), thickness=-1)
         print(center_x,center_y)
 
+    # Draws a green point at the center of the frame
+    cv2.circle(frame0, (int(frame_width/2), int(frame_height/2)), radius=2, color=(0,255,0), thickness=-1)
+    
     # Display the resulting frames
     cv2.imshow('Webcam', frame0)
     # Break the loop on 'q' key press
